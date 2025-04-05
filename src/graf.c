5cdbd1b5e5818e67e7d362bd788b2ded0a7c48d0
Adam Domański <domadam04@gmail.com>
2025-04-05 23:55:49 +0200

    Dodanie kolejnych funkcji grafu

--- src/graf.c
+++ src/graf.c
@@ -35,3 +35,35 @@ void addEdge(Graph* graph, int u, int v)
     newNode->next = graph->adj[v];
     graph->adj[v] = newNode;
 }
+
+void printGraph(Graph * graph)
+{
+    for (int i = 0; i < graph->n; i++)
+    {
+        Node * temp = graph->adj[i];
+        printf("Wierzcholek %d", i);
+        while (temp)
+        {
+            printf("%d ->", temp->vertex);
+            temp = temp->next;
+        }
+        printf("\n");
+
+    }
+}
+
+void freeGraph(Graph * graph)
+{
+    for (int i = 0; i < graph->n; i++)
+    {
+        Node * temp = graph->adj[i];
+        while (temp)
+        {
+            Node * toDelete = temp;
+            temp = temp->next;
+            free(toDelete);
+        }
+    }
+    free(graph->adj);
+    free(graph);
+}
